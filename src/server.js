2bb4f13eb90853392977f666e92359747858b87f
nikunj3619 <gaikwadnikunj3619@gmail.com>
2025-01-02 11:37:20 +0530

    Cors added

--- src/server.js
+++ src/server.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const app = express();
-const NoteRouter = require('./routes/Note')
+const NoteRouter = require('./routes/Note');
+const cors = require('cors'); // Import the CORS package
 require('dotenv').config();
 
+// Middleware
+app.use(express.json());
+app.use(cors()); // Use CORS middleware globally
 
-app.use(express.json())
-
+// MongoDB Connection
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODBURI)
-.then(function(){
-    app.get('/', (req, res)=>{
-        res.send("This is the Home Page");
-    });
-  
-    app.use('/notes', NoteRouter);
-})
+mongoose
+    .connect(process.env.MONGODBURI)
+    .then(function () {
+        console.log('Connected to MongoDB');
+        
+        // Routes
+        app.get('/', (req, res) => {
+            res.send('This is the Home Page');
+        });
 
-const PORT = process.env.PORT || 5000
+        app.use('/notes', NoteRouter);
+    })
+    .catch((err) => console.error('MongoDB connection error:', err));
 
-app.listen(PORT, function() {
-    console.log("App is running on", PORT);
+// Server Listener
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function () {
+    console.log('App is running on port', PORT);
 });
